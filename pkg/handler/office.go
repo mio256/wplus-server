6456a2407465dbdd3924881236e3081e1504cf32
mio256 <reiya@woorth.io>
2024-02-18 22:08:26 +0900

    :sparkles: handler; office

---
+++ pkg/handler/office.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mio256/wplus-server/pkg/infra"
+	"github.com/mio256/wplus-server/pkg/infra/rdb"
+	"github.com/taxio/errors"
+)
+
+func PostOffice(c *gin.Context) {
+	dbConn := infra.ConnectDB(c)
+	repo := rdb.New(dbConn)
+
+	var input struct {
+		Name string `json:"name"`
+	}
+	if err := c.BindJSON(&input); err != nil {
+		c.Error(errors.Wrap(err))
+		return
+	}
+
+	office, err := repo.CreateOffice(c, input.Name)
+	if err != nil {
+		c.Error(errors.Wrap(err))
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, office)
+}
+
+func DeleteOffice(c *gin.Context) {
+	dbConn := infra.ConnectDB(c)
+	repo := rdb.New(dbConn)
+
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.Error(errors.Wrap(err))
+		return
+	}
+
+	if err := repo.SoftDeleteOffice(c, id); err != nil {
+		c.Error(errors.Wrap(err))
+		return
+	}
+
+	c.Status(http.StatusNoContent)
+}
