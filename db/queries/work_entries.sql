929a3642960e1d4a9202e7158dbb09291b907088
mio256 <reiya@woorth.io>
2024-02-18 22:07:07 +0900

    :sparkles: queries; work_entries

---
+++ db/queries/work_entries.sql
@@ -0,0 +1,7 @@
+-- name: CreateWorkEntry :one
+insert into work_entries (employee_id, workplace_id, date, hours, start_time, end_time, attendance, comment)
+values ($1, $2, $3, $4, $5, $6, $7, $8)
+returning *;
+
+-- name: SoftDeleteWorkEntry :exec
+update work_entries set deleted_at = now() where id = $1;
