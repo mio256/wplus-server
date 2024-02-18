0fd5884cc608b0e815cdd9f5059c1cb21ab7cd88
mio256 <reiya@woorth.io>
2024-02-18 22:09:40 +0900

    :sparkles: test; employee

---
+++ pkg/test/employee.go
@@ -0,0 +1,49 @@
+package test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/go-faker/faker/v4"
+	"github.com/mio256/wplus-server/pkg/infra/rdb"
+	"github.com/stretchr/testify/require"
+)
+
+func CreateEmployee(t *testing.T, ctx context.Context, db rdb.DBTX, f func(v *rdb.Employee)) *rdb.Employee {
+	t.Helper()
+
+	wp := CreateWorkplace(t, ctx, db, nil)
+
+	target := &rdb.Employee{
+		Name:        faker.Username(),
+		WorkplaceID: wp.ID,
+	}
+
+	if f != nil {
+		f(target)
+	}
+
+	created, err := rdb.New(db).TestCreateEmployee(ctx, rdb.TestCreateEmployeeParams{
+		Name:        target.Name,
+		WorkplaceID: target.WorkplaceID,
+	})
+
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		require.NoError(t, rdb.New(db).TestDeleteEmployee(ctx, created.ID))
+	})
+
+	return &created
+}
+
+func CheckDeletedEmployee(t *testing.T, ctx context.Context, db rdb.DBTX, id int64) time.Time {
+	t.Helper()
+
+	deletedAt, err := rdb.New(db).TestCheckDeletedEmployee(ctx, id)
+	require.NoError(t, err)
+	require.Equal(t, true, deletedAt.Valid)
+
+	return deletedAt.Time
+}
