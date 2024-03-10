903f7200f023a94cc4ab31983fa71ebcdc1663ce
mio256 <reiya@woorth.io>
2024-03-10 22:41:48 +0900

    :fire: main; setup server in localIP

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"net"
+	"os"
+
 	"github.com/mio256/wplus-server/pkg/ui"
 )
 
 func main() {
 	r := ui.SetupRouter()
-	r.Run(":8080")
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				r.Run(fmt.Sprint(ipnet.IP.String(), ":8080"))
+			}
+		}
+	}
 }
