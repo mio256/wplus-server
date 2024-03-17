87b59867f8c6044b4089320814a418b641caa93a
mio256 <reiya@woorth.io>
2024-03-17 19:02:05 +0900

    :test_tube: support auth in employee_test

--- pkg/handler/employee_test.go
+++ pkg/handler/employee_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/mio256/wplus-server/pkg/infra/rdb"
 	"github.com/mio256/wplus-server/pkg/test"
 	"github.com/mio256/wplus-server/pkg/ui"
+	"github.com/mio256/wplus-server/pkg/util"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -30,20 +31,19 @@ func TestGetEmployees(t *testing.T) {
 		v.WorkplaceID = wp.ID
 	})
 
-	c.Request, _ = http.NewRequest("GET", fmt.Sprintf("%s/%d", ui.EmployeePath, wp.ID), nil)
+	token, err := util.GenerateToken(uint64(test.CreateUser(t, c, dbConn, nil).ID))
+	require.NoError(t, err)
+	c.Request, err = http.NewRequest("GET", fmt.Sprintf("%s/%d", ui.EmployeePath, wp.ID), nil)
+	require.NoError(t, err)
+	c.Request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 	router.ServeHTTP(w, c.Request)
 
-	assert.Equal(t, w.Code, 200)
+	assert.Equal(t, 200, w.Code)
 	var res []rdb.Employee
 	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &res))
 	assert.NotEmpty(t, res)
-	assert.Equal(t, res[0].Name, e.Name)
-	assert.Equal(t, res[0].WorkplaceID, e.WorkplaceID)
-
-	t.Cleanup(func() {
-		require.NoError(t, rdb.New(dbConn).TestDeleteEmployee(c, res[0].ID))
-		require.NoError(t, rdb.New(dbConn).TestDeleteWorkplace(c, wp.ID))
-	})
+	assert.Equal(t, e.Name, res[0].Name)
+	assert.Equal(t, e.WorkplaceID, res[0].WorkplaceID)
 }
 
 func TestGetEmployee(t *testing.T) {
@@ -58,20 +58,19 @@ func TestGetEmployee(t *testing.T) {
 		v.WorkplaceID = wp.ID
 	})
 
-	c.Request, _ = http.NewRequest("GET", fmt.Sprintf("%s/%d/%d", ui.EmployeePath, wp.ID, e.ID), nil)
+	token, err := util.GenerateToken(uint64(test.CreateUser(t, c, dbConn, nil).ID))
+	require.NoError(t, err)
+	c.Request, err = http.NewRequest("GET", fmt.Sprintf("%s/%d/%d", ui.EmployeePath, wp.ID, e.ID), nil)
+	require.NoError(t, err)
+	c.Request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 	router.ServeHTTP(w, c.Request)
 
-	assert.Equal(t, w.Code, 200)
+	assert.Equal(t, 200, w.Code)
 	var res rdb.Employee
 	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &res))
 	assert.NotEmpty(t, res)
-	assert.Equal(t, res.Name, e.Name)
-	assert.Equal(t, res.WorkplaceID, e.WorkplaceID)
-
-	t.Cleanup(func() {
-		require.NoError(t, rdb.New(dbConn).TestDeleteEmployee(c, res.ID))
-		require.NoError(t, rdb.New(dbConn).TestDeleteWorkplace(c, wp.ID))
-	})
+	assert.Equal(t, e.Name, res.Name)
+	assert.Equal(t, e.WorkplaceID, res.WorkplaceID)
 }
 
 func TestPostEmployee(t *testing.T) {
@@ -88,9 +87,13 @@ func TestPostEmployee(t *testing.T) {
 	}
 	b, err := json.Marshal(p)
 	require.NoError(t, err)
-
 	body := bytes.NewBuffer(b)
-	c.Request, _ = http.NewRequest("POST", ui.EmployeePath, body)
+
+	token, err := util.GenerateToken(uint64(test.CreateUser(t, c, dbConn, nil).ID))
+	require.NoError(t, err)
+	c.Request, err = http.NewRequest("POST", ui.EmployeePath, body)
+	require.NoError(t, err)
+	c.Request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 	router.ServeHTTP(w, c.Request)
 
 	assert.Equal(t, w.Code, 200)
@@ -112,13 +115,13 @@ func TestDeleteEmployee(t *testing.T) {
 
 	created := test.CreateEmployee(t, c, dbConn, nil)
 
-	c.Request, _ = http.NewRequest("DELETE", fmt.Sprintf("%s/%d", ui.EmployeePath, created.ID), nil)
+	token, err := util.GenerateToken(uint64(test.CreateUser(t, c, dbConn, nil).ID))
+	require.NoError(t, err)
+	c.Request, err = http.NewRequest("DELETE", fmt.Sprintf("%s/%d", ui.EmployeePath, created.ID), nil)
+	require.NoError(t, err)
+	c.Request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 	router.ServeHTTP(w, c.Request)
 
 	assert.Equal(t, 204, w.Code)
 	assert.NotEmpty(t, test.CheckDeletedEmployee(t, c, dbConn, created.ID))
-
-	t.Cleanup(func() {
-		require.NoError(t, rdb.New(dbConn).TestDeleteEmployee(c, created.ID))
-	})
 }
