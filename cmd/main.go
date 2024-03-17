56600bfe0af1721f9b304171d10dcdbb592430fe
mio256 <reiya@woorth.io>
2024-03-17 19:03:02 +0900

    :sparkles: cmd; main

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	ctx := context.Background()
+	if err := rootCmd(ctx).Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func rootCmd(ctx context.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "cli",
+	}
+
+	cmd.AddCommand(
+		serverCmd(ctx),
+		userSubCmd(ctx),
+	)
+
+	return cmd
+}
