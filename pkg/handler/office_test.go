97e45b3422ad61e013cb18a7cdf5fe479576e806
mio256 <reiya@woorth.io>
2024-03-17 19:02:12 +0900

    :test_tube: support auth in office_test

--- pkg/handler/office_test.go
+++ pkg/handler/office_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/mio256/wplus-server/pkg/infra/rdb"
 	"github.com/mio256/wplus-server/pkg/test"
 	"github.com/mio256/wplus-server/pkg/ui"
+	"github.com/mio256/wplus-server/pkg/util"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -26,18 +27,18 @@ func TestGetOffices(t *testing.T) {
 
 	created := test.CreateOffice(t, c, dbConn, nil)
 
-	c.Request, _ = http.NewRequest("GET", ui.OfficePath, nil)
+	token, err := util.GenerateToken(uint64(test.CreateUser(t, c, dbConn, nil).ID))
+	require.NoError(t, err)
+	c.Request, err = http.NewRequest("GET", ui.OfficePath, nil)
+	require.NoError(t, err)
+	c.Request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 	router.ServeHTTP(w, c.Request)
 
-	assert.Equal(t, w.Code, 200)
+	assert.Equal(t, 200, w.Code)
 	var res []rdb.Office
 	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &res))
 	assert.NotEmpty(t, res)
 	assert.Contains(t, res, *created)
-
-	t.Cleanup(func() {
-		require.NoError(t, rdb.New(dbConn).TestDeleteOffice(c, created.ID))
-	})
 }
 
 func TestPostOffice(t *testing.T) {
@@ -52,15 +53,19 @@ func TestPostOffice(t *testing.T) {
 	p.Name = faker.Username()
 	b, err := json.Marshal(p)
 	require.NoError(t, err)
-
 	body := bytes.NewBuffer(b)
-	c.Request, _ = http.NewRequest("POST", ui.OfficePath, body)
+
+	token, err := util.GenerateToken(uint64(test.CreateUser(t, c, dbConn, nil).ID))
+	require.NoError(t, err)
+	c.Request, err = http.NewRequest("POST", ui.OfficePath, body)
+	require.NoError(t, err)
+	c.Request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 	router.ServeHTTP(w, c.Request)
 
-	assert.Equal(t, w.Code, 200)
+	assert.Equal(t, 200, w.Code)
 	var res rdb.Office
 	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &res))
-	assert.Equal(t, res.Name, p.Name)
+	assert.Equal(t, p.Name, res.Name)
 
 	t.Cleanup(func() {
 		require.NoError(t, rdb.New(dbConn).TestDeleteOffice(c, res.ID))
@@ -75,13 +80,13 @@ func TestDeleteOffice(t *testing.T) {
 
 	created := test.CreateOffice(t, c, dbConn, nil)
 
-	c.Request, _ = http.NewRequest("DELETE", fmt.Sprintf("%s/%d", ui.OfficePath, created.ID), nil)
+	token, err := util.GenerateToken(uint64(test.CreateUser(t, c, dbConn, nil).ID))
+	require.NoError(t, err)
+	c.Request, err = http.NewRequest("DELETE", fmt.Sprintf("%s/%d", ui.OfficePath, created.ID), nil)
+	require.NoError(t, err)
+	c.Request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 	router.ServeHTTP(w, c.Request)
 
 	assert.Equal(t, 204, w.Code)
 	assert.NotEmpty(t, test.CheckDeletedOffice(t, c, dbConn, created.ID))
-
-	t.Cleanup(func() {
-		require.NoError(t, rdb.New(dbConn).TestDeleteOffice(c, created.ID))
-	})
 }
