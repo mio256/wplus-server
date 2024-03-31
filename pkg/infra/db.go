88fd1faf64229bba1ca9c00a278f2888063feafa
mio256 <reiya@woorth.io>
2024-03-31 13:57:32 +0900

    :sparkles: infra.ConnectDB; support gcloud sql

--- pkg/infra/db.go
+++ pkg/infra/db.go
@@ -2,16 +2,34 @@ package infra
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/mio256/wplus-server/pkg/infra/rdb"
 )
 
 func ConnectDB(ctx context.Context) *pgxpool.Pool {
-	dbConn, err := pgxpool.New(ctx, os.Getenv("DATABASE_URL"))
+	var (
+		dbUser         = os.Getenv("DB_USER")              // e.g. 'my-db-user'
+		dbPwd          = os.Getenv("DB_PASS")              // e.g. 'my-db-password'
+		unixSocketPath = os.Getenv("INSTANCE_UNIX_SOCKET") // e.g. '/cloudsql/project:region:instance'
+		dbName         = os.Getenv("DB_NAME")              // e.g. 'my-database'
+	)
+
+	dbConn, err := pgxpool.New(ctx, fmt.Sprintf("user=%s password=%s database=%s host=%s", dbUser, dbPwd, dbName, unixSocketPath))
 	if err != nil {
 		panic(err)
 	}
 
 	return dbConn
+}
+
+func CheckConnectDB(ctx context.Context) {
+	dbConn := ConnectDB(ctx)
+	repo := rdb.New(dbConn)
+	if err := repo.Ping(ctx); err != nil {
+		panic(err)
+	}
+	defer dbConn.Close()
 }
