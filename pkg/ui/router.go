1f9613742efea09bb2235f83df89cd261d40d4dc
mio256 <reiya@woorth.io>
2024-03-31 13:55:12 +0900

    :sparkles: db-ping; infra.CheckConnectDB

--- pkg/ui/router.go
+++ pkg/ui/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/mio256/wplus-server/pkg/handler"
+	"github.com/mio256/wplus-server/pkg/infra"
 	"github.com/mio256/wplus-server/pkg/util"
 )
 
@@ -19,9 +20,16 @@ func SetupRouter() *gin.Engine {
 	r.Use(cors.Default())
 
 	// ping
-	r.GET("/ping", func(context *gin.Context) {
-		context.JSON(200, gin.H{
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
 			"message": "pong",
+		})
+	})
+	// db-ping
+	r.GET("/db-ping", func(ctx *gin.Context) {
+		infra.CheckConnectDB(ctx)
+		ctx.JSON(200, gin.H{
+			"message": "db-pong",
 		})
 	})
 
