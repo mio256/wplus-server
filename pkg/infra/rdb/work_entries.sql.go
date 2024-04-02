da2ecdd4f43fdc3b38f4e6e48187852888642212
mio256 <reiya@woorth.io>
2024-04-02 13:29:03 +0900

    :zap::fire: db/queires; GetWorkEntriesByEmployee, GetWorkEntriesByWorkplace

--- pkg/infra/rdb/work_entries.sql.go
+++ pkg/infra/rdb/work_entries.sql.go
@@ -58,19 +58,42 @@ func (q *Queries) CreateWorkEntry(ctx context.Context, arg CreateWorkEntryParams
 }
 
 const getWorkEntriesByEmployee = `-- name: GetWorkEntriesByEmployee :many
-select id, employee_id, workplace_id, date, hours, start_time, end_time, attendance, comment, deleted_at, created_at, updated_at from work_entries where employee_id = $1 and deleted_at is null
+select workplaces.name as workplace_name, employees.name as employee_name, work_entries.id, work_entries.employee_id, work_entries.workplace_id, work_entries.date, work_entries.hours, work_entries.start_time, work_entries.end_time, work_entries.attendance, work_entries.comment, work_entries.deleted_at, work_entries.created_at, work_entries.updated_at
+from work_entries
+join employees on work_entries.employee_id = employees.id
+join workplaces on work_entries.workplace_id = workplaces.id
+where employees.id = $1 and work_entries.deleted_at is null
 `
 
-func (q *Queries) GetWorkEntriesByEmployee(ctx context.Context, employeeID int64) ([]WorkEntry, error) {
-	rows, err := q.db.Query(ctx, getWorkEntriesByEmployee, employeeID)
+type GetWorkEntriesByEmployeeRow struct {
+	WorkplaceName string           `json:"workplace_name"`
+	EmployeeName  string           `json:"employee_name"`
+	ID            int64            `json:"id"`
+	EmployeeID    int64            `json:"employee_id"`
+	WorkplaceID   int64            `json:"workplace_id"`
+	Date          pgtype.Date      `json:"date"`
+	Hours         pgtype.Int2      `json:"hours"`
+	StartTime     pgtype.Time      `json:"start_time"`
+	EndTime       pgtype.Time      `json:"end_time"`
+	Attendance    pgtype.Bool      `json:"attendance"`
+	Comment       pgtype.Text      `json:"comment"`
+	DeletedAt     pgtype.Timestamp `json:"deleted_at"`
+	CreatedAt     pgtype.Timestamp `json:"created_at"`
+	UpdatedAt     pgtype.Timestamp `json:"updated_at"`
+}
+
+func (q *Queries) GetWorkEntriesByEmployee(ctx context.Context, id int64) ([]GetWorkEntriesByEmployeeRow, error) {
+	rows, err := q.db.Query(ctx, getWorkEntriesByEmployee, id)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
-	var items []WorkEntry
+	var items []GetWorkEntriesByEmployeeRow
 	for rows.Next() {
-		var i WorkEntry
+		var i GetWorkEntriesByEmployeeRow
 		if err := rows.Scan(
+			&i.WorkplaceName,
+			&i.EmployeeName,
 			&i.ID,
 			&i.EmployeeID,
 			&i.WorkplaceID,
@@ -129,6 +152,66 @@ func (q *Queries) GetWorkEntriesByOffice(ctx context.Context, id int64) ([]GetWo
 	var items []GetWorkEntriesByOfficeRow
 	for rows.Next() {
 		var i GetWorkEntriesByOfficeRow
+		if err := rows.Scan(
+			&i.WorkplaceName,
+			&i.EmployeeName,
+			&i.ID,
+			&i.EmployeeID,
+			&i.WorkplaceID,
+			&i.Date,
+			&i.Hours,
+			&i.StartTime,
+			&i.EndTime,
+			&i.Attendance,
+			&i.Comment,
+			&i.DeletedAt,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+const getWorkEntriesByWorkplace = `-- name: GetWorkEntriesByWorkplace :many
+select workplaces.name as workplace_name, employees.name as employee_name, work_entries.id, work_entries.employee_id, work_entries.workplace_id, work_entries.date, work_entries.hours, work_entries.start_time, work_entries.end_time, work_entries.attendance, work_entries.comment, work_entries.deleted_at, work_entries.created_at, work_entries.updated_at
+from work_entries
+join employees on work_entries.employee_id = employees.id
+join workplaces on work_entries.workplace_id = workplaces.id
+where workplaces.id = $1 and work_entries.deleted_at is null
+`
+
+type GetWorkEntriesByWorkplaceRow struct {
+	WorkplaceName string           `json:"workplace_name"`
+	EmployeeName  string           `json:"employee_name"`
+	ID            int64            `json:"id"`
+	EmployeeID    int64            `json:"employee_id"`
+	WorkplaceID   int64            `json:"workplace_id"`
+	Date          pgtype.Date      `json:"date"`
+	Hours         pgtype.Int2      `json:"hours"`
+	StartTime     pgtype.Time      `json:"start_time"`
+	EndTime       pgtype.Time      `json:"end_time"`
+	Attendance    pgtype.Bool      `json:"attendance"`
+	Comment       pgtype.Text      `json:"comment"`
+	DeletedAt     pgtype.Timestamp `json:"deleted_at"`
+	CreatedAt     pgtype.Timestamp `json:"created_at"`
+	UpdatedAt     pgtype.Timestamp `json:"updated_at"`
+}
+
+func (q *Queries) GetWorkEntriesByWorkplace(ctx context.Context, id int64) ([]GetWorkEntriesByWorkplaceRow, error) {
+	rows, err := q.db.Query(ctx, getWorkEntriesByWorkplace, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []GetWorkEntriesByWorkplaceRow
+	for rows.Next() {
+		var i GetWorkEntriesByWorkplaceRow
 		if err := rows.Scan(
 			&i.WorkplaceName,
 			&i.EmployeeName,
