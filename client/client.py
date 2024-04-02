4a47556ba82da1015095772fe5ba0bd3c5b3d31c
mio256 <reiya@woorth.io>
2024-04-02 17:32:17 +0900

    :sparkles: client; access routing

--- client/client.py
+++ client/client.py
@@ -1,51 +1,177 @@
+import os
 import requests
-import json
-from datetime import datetime
 
-# この2つは固定
-base_url = 'https://wplus-service-7gmfn2s4tq-dt.a.run.app/' # デプロイ先のURL
+# 環境変数から基本URLを取得
+base_url = os.environ['BASE_URL']
 headers = {'Content-Type': 'application/json'}
 
-# serverの生存確認
-response = requests.get(base_url+'/ping', headers=headers)
-print(response.json())  # {'message': 'pong'}
 
-# dbの生存確認
-response = requests.get(base_url+'/db-ping', headers=headers)
-print(response.json())  # {'message': 'db-pong'}
+def ping_server():
+    """サーバーの生存確認を行う"""
+    response = requests.get(f'{base_url}/ping', headers=headers)
+    if response.ok:
+        print('ping: ', response.json())
+    else:
+        print('Error pinging server')
 
-# ログインフォームから取得してくる
-data = {
-    'office_id': int(input('office_id: ')),
-    'user_id': int(input('user_id: ')),
-    'password': input('password: ')
-}
 
-response = requests.post(base_url+'/login', headers=headers, json=data)
-print(response.json())  # {'name': 'yamada', 'office_id': 1, 'role': 'admin', 'user_id': 1} → roleを使って画面遷移を制御する
-token = response.cookies.get('token')  # JWTトークンを取得
-print(token)
-login_res = response.json()
+def ping_database():
+    """データベースの生存確認を行う"""
+    response = requests.get(f'{base_url}/db-ping', headers=headers)
+    if response.ok:
+        print('db-ping: ', response.json())
+    else:
+        print('Error pinging database')
 
-r = requests.get(base_url+'/offices', headers={'Authorization': 'Bearer '+token})  # トークンを使ってリクエストを送信
-print(r.json())
 
-# Assuming login_res and token are defined
-work_entry_data = {
-    "employee_id": login_res['employee_id'],
-    "workplace_id": login_res['workplace_id'],
-    "date": "2006-01-02T00:00:00.000000Z", # 日付しか使わないので、時間は00:00:00にしておく
-    "start_time": "1970-01-01T08:00:00.000000Z", # 時間しか使わないので、日付は1970-01-01にしておく (UNXITime=0からの経過時間
-    "end_time": "1970-01-01T17:00:00.000000Z", # 時間しか使わないので、日付は1970-01-01にしておく
-}
+def login():
+    """ログイン処理を行い、トークンとログインレスポンスを取得する"""
+    data = {
+        'office_id': int(input('office_id: ')),
+        'user_id': int(input('user_id: ')),
+        'password': input('password: ')
+    }
+    response = requests.post(f'{base_url}/login', headers=headers, json=data)
+    if response.ok:
+        print('login: ', response.json(), response.cookies.get('token'))
+        return response.cookies.get('token'), response.json()
+    else:
+        print('Login failed')
+        return None, None
 
-print(work_entry_data)
 
-# Assuming base_url is defined
-r = requests.post(base_url + '/work_entries', headers={'Authorization': 'Bearer ' + token}, json=work_entry_data)
-print(r.status_code)
-try:
-    print(r.json())
-except json.JSONDecodeError:
-    print("Response is not in JSON format:")
-    print(r.text)
+def get_workplace(workplace_id):
+    return requests.get(base_url + f'/workplaces/{workplace_id}', headers=headers).json()
+
+
+def get_employees(role, id):
+    # role = 'admin', 'manager', 'employee'
+    # id = office_id, workplace_id, employee_id
+    if role == 'admin':
+        r = requests.get(base_url + f'/employees/office/{id}', headers=headers)
+    elif role == 'manager':
+        r = requests.get(base_url + f'/employees/workplace/{id}', headers=headers)
+    elif role == 'employee':
+        r = requests.get(base_url + f'/employees/{id}', headers=headers)
+    return r.json()
+
+
+def post_work_entry(employee_id, workplace_id):
+    work_entry_data = {
+        "employee_id": employee_id,
+        "workplace_id": workplace_id
+    }
+    date = input('date (yyyy-mm-dd): ')
+    work_entry_data['date'] = date + 'T00:00:00.000Z'  # 日付しか使わないので、時間は00:00:00にしておく
+
+    # workplace_typeを取得
+    """ CHECK:
+    adminが勤怠登録画面に行く場合、workplace_idがNullなのでworkplace.typeが取得できず、勤怠登録画面を「時間数」「有無」「時間（star,end）」の切り替えを動的にしないといけませんね。
+    例えば、adminが勤怠登録画面にいくと「職場選択」→「従業員選択」→入力フォーム のように画面が遷移するなど。
+    今はstart,endだけなので、workplace_idがNullだろうと決め打ちでworkplace.type='time'でstart,endの画面固定でよいです。
+    """
+    workplace_type = get_workplace(workplace_id)['work_type']
+
+    # workplace_typeによって、勤怠登録の方法を変える
+    if workplace_type == 'hours':
+        work_entry_data['hours'] = int(input('hours: '))
+    elif workplace_type == 'time':
+        start_time = input('start_time (hh:mm): ')
+        end_time = input('end_time (hh:mm): ')
+        work_entry_data['start_time'] = '1970-01-01T' + start_time + ':00.000Z'  # 時間しか使わないので、日付はUnixTimeの0秒の日付=1970-01-01にしておく
+        work_entry_data['end_time'] = '1970-01-01T' + end_time + ':00.000Z'  # 時間しか使わないので、日付はUnixTimeの0秒の日付=1970-01-01にしておく
+    elif workplace_type == 'attendance':
+        work_entry_data['attendance'] = bool(input('attendance (y/n): ') == 'y')
+
+    r = requests.post(base_url + '/work_entries', headers=headers, json=work_entry_data)
+    return r.json()
+
+
+def get_work_entries(role, id):
+    # role = 'admin', 'manager', 'employee'
+    # id = office_id, workplace_id, employee_id
+    if role == 'admin':
+        r = requests.get(base_url + f'/work_entries/office/{id}', headers=headers)
+    elif role == 'manager':
+        r = requests.get(base_url + f'/work_entries/workplace/{id}', headers=headers)
+    elif role == 'employee':
+        r = requests.get(base_url + f'/work_entries/employee/{id}', headers=headers)
+    return r.json()
+
+
+def main():
+    ping_server()
+    ping_database()
+
+    token, login_res = login()
+
+    # tokenとlogin_resがNoneでない場合、ログイン成功
+    if token and login_res:
+        # Tokenをヘッダーに追加
+        headers['Authorization'] = 'Bearer ' + token
+
+        # ログインレスポンスからroleを取得
+        role = login_res['role']
+        print('your role: ', role)
+
+        # yesならば、勤怠登録画面
+        # noならば、勤怠一覧画面
+        post = bool(input('post work_entry? (y/n): ') == 'y')
+
+        if role == 'admin':
+            office_id = login_res['office_id']
+            workplace_id = None
+            # adminはworkplace_idがNullなので注意 (office:workplace = 1:N)
+            if post:
+                # 管理者はOffice全体から対象のEmployeeを選択して勤怠登録
+
+                # 従業員一覧
+                employees = get_employees(role, office_id)
+                for _, employee in enumerate(employees):
+                    print(employee)
+
+                # 従業員のIDを選択
+                employee_id = int(input('employee_id: '))
+                # 指定させたemployeeからworkplace_idを取得する
+                for _, employee in enumerate(employees):
+                    if employee['id'] == employee_id:
+                        workplace_id = employee['workplace_id']
+
+                print(post_work_entry(employee_id, workplace_id))
+            else:
+                # 管理者はOffice全体の勤怠一覧を表示
+                print(get_work_entries(role, office_id))
+        elif role == 'manager':
+            office_id = login_res['office_id']
+            workplace_id = login_res['workplace_id']
+            if post:
+                # マネージャーは自分のWorkplaceのEmployeeを選択して勤怠登録
+
+                # 従業員一覧
+                employees = get_employees(role, workplace_id)
+                print(employees)
+
+                # 従業員のIDを選択
+                employee_id = int(input('employee_id: '))
+
+                # managerは自分のworkplace_idを使う
+                print(post_work_entry(employee_id, workplace_id))
+            else:
+                # マネージャーは自分のWorkplaceの勤怠一覧を表示
+                print(get_work_entries(role, workplace_id))
+        elif role == 'employee':
+            office_id = login_res['office_id']
+            workplace_id = login_res['workplace_id']
+            employee_id = login_res['employee_id']
+            if post:
+                # 従業員は自分の勤怠登録
+                # employee は 画面表示用の情報として取得、employee.nameなど
+                employee = get_employees(role, employee_id)
+                print(post_work_entry(employee_id, workplace_id))
+            else:
+                # 従業員は自分の勤怠一覧を表示
+                print(get_work_entries(role, employee_id))
+
+
+if __name__ == "__main__":
+    main()
